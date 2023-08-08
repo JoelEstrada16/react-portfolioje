dd972ebac60734608d0f3c0f546883272bdfbf2f
JoelEstrada16 <joey220.je@gmail.com>
2023-08-07 20:48:25 -0700

    added resume and added knowledge for the email and the validation for the email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,53 @@
 import './Contact.css'
-const Contact = () => {
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
+
+function Contact() { 
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+        console.log('Submit Form', formState);
+    }
+} 
+const handleChange = (e) => {
+    if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        console.log(isValid);
+        if (!isValid) {
+            setErrorMessage('Your email is invalid.');
+        } else {
+            setErrorMessage('');
+        }
+    } else {
+        if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+        } else {
+            setErrorMessage('');
+        }
+    }
+    if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+}     
     return (
+    <>
+   
         <div class="backgroundcolor">
-            <h1>Contact</h1>
-            <p>Contact Page content</p>
+            <form>
+                <label className='inputs'>
+                    name
+                    <input type="text" name="name"/>
+                    email
+                    <input type="text" name="email"/>
+                    message
+                    <input type="text" name="message"/>
+                </label>
+            </form>
         </div>
+    </>
     )
 };
 
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -2,8 +2,28 @@ import './resume.css'
 const Resume = () => {
     return (
         <div class="backgroundcolor">
-            <h1>Resume</h1>
-            <p>Resume Page content</p>
+            <form>
+                <h1 className='h1'>Front End</h1>
+                <ol>
+                <ul>REACT</ul>
+                <ul>HTML</ul>
+                <ul>CSS</ul>
+                <ul>JAVASCRIPT</ul>
+                <ul>JQUERY</ul>
+                <ul>BOOTSTRAO</ul>
+                <ul>RESPONSIVE DESIGN</ul>
+                </ol>
+                <h1 className='h1'>Back End</h1>
+                <ol>
+                    <ul>API'S</ul>
+                    <ul>NODE</ul>
+                    <ul>EXPRESS</ul>
+                    <ul>MYSQL, SEQUELIZE</ul>
+                    <ul>MONGODB, MONGOOSE</ul>
+                    <ul>REST</ul>
+                    <ul>GRAPHQL</ul>
+                </ol>
+            </form>
         </div>
     )
 };
